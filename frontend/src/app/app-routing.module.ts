3d544f6f2d154fd956004b3706540406a494d3af
Juan Duque <juanpablo.duque@polimi.it>
2022-12-02 12:12:25 +0100

    added cesium

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Map3dComponent } from './features/cesium/map3d/map3d.component';
 import { MapComponent } from './features/map/pages/map/map.component';
 
 const routes: Routes = [
   {
     path: '', component: MapComponent
   },
+
+  {
+    path: 'cesium', component: Map3dComponent
+  }
 
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { CesiumDirective } from './features/cesium/directive/cesium.directive';
+import { Map3dComponent } from './features/cesium/map3d/map3d.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CesiumDirective,
+    Map3dComponent
   ],
   imports: [
     BrowserModule,
